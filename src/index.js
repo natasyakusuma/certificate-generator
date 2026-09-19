9bfcae08448aa7af952dfed798749f8837ae3253
Natasya <natasyakusuma94@gmail.com>
2026-09-19 15:12:43 +0700

    Use secret file for Google credentials

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {readFile} from "fs/promises";
 import { pejabat } from "../config/pejabat.js";
 
 const auth = new google.auth.GoogleAuth({
-  credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
+  keyFile: "/etc/secrets/service-account.json",
   scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
 });
 
